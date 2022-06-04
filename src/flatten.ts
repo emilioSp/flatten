1eca937fcf2fa363c18146a2e4f27f69dc73a1f1
emiliosp <emilio.spatola@gmail.com>
2022-06-04 12:01:51 +0200

    iterative approach

--- src/flatten.ts
+++ src/flatten.ts
@@ -1,12 +1,25 @@
+const flatOneLevel = (arr: Array<any>): Array<any> => {
+  return arr.reduce((acc, value) => {
+    if (Array.isArray(value)) {
+      acc.push(...value);
+    } else {
+      acc.push(value);
+    }
+    return acc;
+  }, []);
+};
+
+const hasNestedLevel = (arr: Array<any>) => arr.some(Array.isArray);
+
 const flatten = (arr: Array<any>): Array<any> => {
   // Type check reinforcement, to avoid runtime errors.
   if (!Array.isArray(arr)) {
     throw new Error(`Invalid input ${arr} is not an array`);
   }
 
-  const flatArray = [];
-  for (let i = 0; i < arr.length; i++) {
-    Array.isArray(arr[i]) ? flatArray.push(...flatten(arr[i])) : flatArray.push(arr[i]);
+  let flatArray: any[] = arr;
+  while (hasNestedLevel(flatArray)) {
+    flatArray = flatOneLevel(flatArray);
   }
   return flatArray;
 };
